7d310fd28e1d3dc99cd64997081d3012ac63cc08
Valeriia Lomakina <122263940+vlrlmn@users.noreply.github.com>
2025-03-23 18:57:00 +0400

    Update HabitViewModel.swift

--- HabitViewModel.swift
+++ HabitViewModel.swift
@@ -1,10 +1,16 @@
-import Foundation
-
 class HabitViewModel: ObservableObject {
     @Published var habits: [Habit] = []
+    let maxHabits = 5
+
+    func canAddMoreHabits() -> Bool {
+        return habits.count < maxHabits
+    }
 
     func addHabit(_ habit: Habit) {
+        guard canAddMoreHabits() else {
+            print("Максимум привычек достигнут.")
+            return
+        }
         habits.append(habit)
-        // здесь можно сохранить в UserDefaults/CoreData
     }
 }
